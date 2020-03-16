3b3212115f9e11be2624f1ac4420b7d5d872f373
wherkamp <wherkamp@kingtux.me>
2020-03-16 05:57:43 -0400

    Updated Server Module.

--- server/src/main/java/org/kakara/server/Server.java
+++ server/src/main/java/org/kakara/server/Server.java
@@ -1,10 +1,20 @@
 package org.kakara.server;
 
 import org.kakara.core.GameInstance;
-import org.kakara.core.GameType;
+import org.kakara.core.GameTypes;
+import org.kakara.core.crafting.CraftingManager;
+import org.kakara.core.events.EventManager;
 import org.kakara.core.game.GameSettings;
 import org.kakara.core.game.Item;
+import org.kakara.core.game.ItemManager;
 import org.kakara.core.game.ItemStack;
+import org.kakara.core.game.entity.EntityManager;
+import org.kakara.core.mod.ModManager;
+import org.kakara.core.resources.ResourceManager;
+import org.kakara.core.sound.SoundManager;
+import org.kakara.core.world.WorldGenerationManager;
+
+import java.io.File;
 
 public class Server implements GameInstance {
     @Override
@@ -18,7 +28,53 @@ public class Server implements GameInstance {
     }
 
     @Override
-    public GameType getType() {
-        return GameType.SERVER;
+    public ResourceManager getResourceManager() {
+        return null;
     }
+
+    @Override
+    public SoundManager getSoundManager() {
+        return null;
+    }
+
+    @Override
+    public ItemManager getItemManager() {
+        return null;
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return null;
+    }
+
+    @Override
+    public CraftingManager getCraftingManager() {
+        return null;
+    }
+
+    @Override
+    public ModManager getModManager() {
+        return null;
+    }
+
+    @Override
+    public File getWorkingDirectory() {
+        return null;
+    }
+
+    @Override
+    public EventManager getEventManager() {
+        return null;
+    }
+
+    @Override
+    public WorldGenerationManager getWorldGenerationManager() {
+        return null;
+    }
+
+    @Override
+    public GameTypes getType() {
+        return GameTypes.SERVER;
+    }
+
 }

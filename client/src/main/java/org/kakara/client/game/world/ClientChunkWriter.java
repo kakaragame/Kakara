7ed897d267e1f898b6dd33dec666a712dbd95245
Wyatt J Herkamp <wherkamp@kingtux.me>
2020-06-29 18:40:34 -0400

    Grouping Chunk Loads + Chunk Saves

--- client/src/main/java/org/kakara/client/game/world/ClientChunkWriter.java
+++ client/src/main/java/org/kakara/client/game/world/ClientChunkWriter.java
@@ -2,9 +2,11 @@ package org.kakara.client.game.world;
 
 import me.ryandw11.ods.Compression;
 import me.ryandw11.ods.ObjectDataStructure;
+import me.ryandw11.ods.Tag;
 import me.ryandw11.ods.exception.ODSException;
 import me.ryandw11.ods.tags.ObjectTag;
 import org.kakara.client.KakaraGame;
+import org.kakara.client.game.world.io.ChunkIOUtils;
 import org.kakara.core.Kakara;
 import org.kakara.core.exceptions.SaveLoadException;
 import org.kakara.core.utils.CoreFileUtils;
@@ -18,10 +20,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ClientChunkWriter implements ChunkWriter {
     private final ClientWorld world;
@@ -41,13 +43,21 @@ public class ClientChunkWriter implements ChunkWriter {
         return new File(chunkFolder, String.format("_%s_%s_%s_.kchunk", x, y, z));
     }
 
+    private File getChunkFileFromChunkFileLocation(ChunkLocation location) {
+        int x = location.getX();
+        int y = location.getY();
+        int z = location.getZ();
+
+        return new File(chunkFolder, String.format("_%s_%s_%s_.kchunk", x, y, z));
+    }
+
     @Override
     public Chunk getChunkByLocation(ChunkLocation chunkLocation) {
         File chunkFile = getChunkFile(chunkLocation);
         if (!chunkFile.exists()) return new NullChunk(chunkLocation);
 
         ObjectDataStructure ods = new ObjectDataStructure(chunkFile, Compression.GZIP);
-        ObjectTag objectTag=null;
+        ObjectTag objectTag = null;
         try {
             objectTag = ods.getObject(chunkLocation.getX() + "-" + chunkLocation.getY() + "-" + chunkLocation.getZ());
         } catch (ODSException e) {
@@ -68,8 +78,34 @@ public class ClientChunkWriter implements ChunkWriter {
     @Override
     public List<Chunk> getChunksByLocation(List<ChunkLocation> locations) {
         List<Chunk> output = new ArrayList<>();
-        for (ChunkLocation location : locations) {
-            output.add(getChunkByLocation(location));
+        for (Map.Entry<ChunkLocation, Collection<ChunkLocation>> chunkLocationCollectionEntry : ChunkIOUtils.sort(locations).asMap().entrySet()) {
+            ChunkLocation chunkLocation = chunkLocationCollectionEntry.getKey();
+            File chunkFile = getChunkFileFromChunkFileLocation(chunkLocation);
+            if (!chunkFile.exists()) {
+                chunkLocationCollectionEntry.getValue().forEach(chunkLocation1 -> {
+                    output.add(new NullChunk(chunkLocation1));
+                });
+                continue;
+            }
+
+            ObjectDataStructure ods = new ObjectDataStructure(chunkFile, Compression.GZIP);
+            for (ChunkLocation chunkLocation1 : chunkLocationCollectionEntry.getValue()) {
+                ObjectTag objectTag = null;
+                try {
+                    objectTag = ods.getObject(chunkLocation.getX() + "-" + chunkLocation.getY() + "-" + chunkLocation.getZ());
+                } catch (ODSException e) {
+                    KakaraGame.LOGGER.error("Unable to get chunk: " + chunkLocation.toString(), e);
+                    //TODO Cancel World Load
+                }
+                ChunkTag chunkTag = null;
+                if (objectTag instanceof ChunkTag) {
+                    chunkTag = (ChunkTag) objectTag;
+                } else {
+                    output.add(new NullChunk(chunkLocation));
+                    continue;
+                }
+                output.add(chunkTag.getChunk());
+            }
         }
         return output;
     }
@@ -85,9 +121,16 @@ public class ClientChunkWriter implements ChunkWriter {
 
     @Override
     public void writeChunks(List<Chunk> chunks) {
-        //TODO this will impact speed. Improve this later.
-        for (Chunk chunk : chunks) {
-            writeChunk(chunk);
+        for (Map.Entry<ChunkLocation, Collection<Chunk>> entry : ChunkIOUtils.sortByChunk(chunks).asMap().entrySet()) {
+            File chunkFile = getChunkFile(entry.getKey());
+            ObjectDataStructure ods = new ObjectDataStructure(chunkFile, Compression.GZIP);
+            for (Chunk chunk : entry.getValue()) {
+                if (ods.find(getChunkKey(chunk.getLocation())))
+                    ods.delete(getChunkKey(chunk.getLocation()));
+            }
+            List<Tag<?>> chunkTags = new ArrayList<>();
+            entry.getValue().forEach(chunk -> chunkTags.add(new ChunkTag(chunk)));
+            ods.appendAll(chunkTags);
         }
     }
 

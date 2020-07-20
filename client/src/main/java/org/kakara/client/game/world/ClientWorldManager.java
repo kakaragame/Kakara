718d238d4c998ad6233ca8bb74e7170701830d74
Wyatt J Herkamp <wherkamp@kingtux.me>
2020-07-19 21:07:47 -0400

    Implemented WorldManager

---
+++ client/src/main/java/org/kakara/client/game/world/ClientWorldManager.java
@@ -0,0 +1,22 @@
+package org.kakara.client.game.world;
+
+import org.jetbrains.annotations.Nullable;
+import org.kakara.client.game.IntegratedServer;
+import org.kakara.core.world.World;
+import org.kakara.core.world.WorldManager;
+import org.kakara.game.Server;
+
+import java.util.UUID;
+
+public class ClientWorldManager implements WorldManager {
+    private IntegratedServer integratedServer;
+
+    public ClientWorldManager(IntegratedServer integratedServer) {
+        this.integratedServer = integratedServer;
+    }
+
+    @Override
+    public @Nullable World getWorldByUUID(UUID uuid) {
+        return integratedServer.getSave().getWorlds().stream().filter(world -> world.getUUID().equals(uuid)).findFirst().orElse(null);
+    }
+}

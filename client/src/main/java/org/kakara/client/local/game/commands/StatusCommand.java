84f80606c975abdd984037969e501c20fc6019d1
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-02-02 19:13:05 -0500

    improved status command

--- client/src/main/java/org/kakara/client/local/game/commands/StatusCommand.java
+++ client/src/main/java/org/kakara/client/local/game/commands/StatusCommand.java
@@ -1,12 +1,14 @@
 package org.kakara.client.local.game.commands;
 
 
+import org.kakara.core.common.Status;
 import org.kakara.core.common.command.CommandSender;
 import org.kakara.core.common.mod.Mod;
 import org.kakara.game.Server;
 import org.kakara.game.commands.BuiltinCommand;
 
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 public class StatusCommand extends BuiltinCommand {
     private final Server server;
@@ -19,7 +21,9 @@ public class StatusCommand extends BuiltinCommand {
     @Override
     public void execute(String command, String[] arguments, String fullCommand, CommandSender executor) {
         executor.sendMessage("Status");
-        executor.sendMessage("Number of Chunks Loaded: " + server.getPlayerEntity().getLocation().getWorld().get().getChunks().size());
+        executor.sendMessage("Number of Chunks Loaded: " + server.getPlayerEntity().getLocation().getNullableWorld().getChunks().stream().filter(chunk -> {
+            return chunk.getStatus() == Status.LOADED;
+        }).collect(Collectors.toList()).size());
         executor.sendMessage("Loc: " + server.getPlayerEntity().getLocation().toString());
     }
 }

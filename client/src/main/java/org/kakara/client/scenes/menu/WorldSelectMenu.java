fb42008a4dc0a26824b21b65db59d80d11340fdf
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-02-05 18:25:09 -0500

    Added two buttons

--- client/src/main/java/org/kakara/client/scenes/menu/WorldSelectMenu.java
+++ client/src/main/java/org/kakara/client/scenes/menu/WorldSelectMenu.java
@@ -1,21 +1,14 @@
 package org.kakara.client.scenes.menu;
 
 import org.kakara.client.KakaraGame;
-import org.kakara.client.join.LocalJoin;
-import org.kakara.client.local.game.SaveCreator;
-import org.kakara.client.local.game.WorldCreator;
-import org.kakara.core.common.ControllerKey;
-import org.kakara.core.common.modinstance.ModInstance;
-import org.kakara.core.common.modinstance.ModInstanceType;
 import org.kakara.engine.GameHandler;
 import org.kakara.engine.gameitems.Texture;
 import org.kakara.engine.math.Vector2;
 import org.kakara.engine.resources.ResourceManager;
 import org.kakara.engine.scene.AbstractMenuScene;
-import org.kakara.engine.ui.components.Sprite;
+import org.kakara.engine.scene.Scene;
 import org.kakara.engine.ui.components.shapes.Rectangle;
 import org.kakara.engine.ui.components.text.Text;
-import org.kakara.engine.ui.constraints.GridConstraint;
 import org.kakara.engine.ui.constraints.HorizontalCenterConstraint;
 import org.kakara.engine.ui.constraints.VerticalCenterConstraint;
 import org.kakara.engine.ui.events.UIClickEvent;
@@ -26,11 +19,6 @@ import org.kakara.engine.ui.font.TextAlign;
 import org.kakara.engine.ui.items.ComponentCanvas;
 import org.kakara.engine.utils.RGBA;
 import org.kakara.engine.window.WindowIcon;
-
-import java.io.File;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
 
 public class WorldSelectMenu extends AbstractMenuScene {
     private final KakaraGame kakaraGame;
@@ -57,8 +45,50 @@ public class WorldSelectMenu extends AbstractMenuScene {
 
 
         ComponentCanvas componentCanvas = new ComponentCanvas(this);
+        {
+            Rectangle backButton = new Rectangle(new Vector2(135, 600), new Vector2(300, 60), new RGBA(255, 255, 255, 0.5f));
+            //backButton.addConstraint(new HorizontalCenterConstraint());
+            Text backButtonText = new Text("Back", roboto);
+            backButtonText.setSize(40);
+            backButtonText.setLineWidth(300);
+            backButtonText.setTextAlign(TextAlign.CENTER | TextAlign.MIDDLE);
+            backButtonText.addConstraint(new VerticalCenterConstraint());
+            backButtonText.addConstraint(new HorizontalCenterConstraint());
+            backButton.add(backButtonText);
 
+            backButton.addUActionEvent(UIHoverEnterEvent.class, (UIHoverEnterEvent) vector2 -> backButton.setColor(new RGBA(204, 202, 202, 0.5f)));
+            backButton.addUActionEvent(UIHoverLeaveEvent.class, (UIHoverLeaveEvent) vector2 -> backButton.setColor(new RGBA(255, 255, 255, 0.5f)));
+            backButton.addUActionEvent(UIClickEvent.class, (UIClickEvent) (vector2, mouseClickType) -> goBack(backButton));
 
+            componentCanvas.add(backButton);
+        }
+        {
+            Rectangle createWorld = new Rectangle(new Vector2(600, 600), new Vector2(300, 60), new RGBA(255, 255, 255, 0.5f));
+            Text backButtonText = new Text("Create New World", roboto);
+            backButtonText.setSize(40);
+            backButtonText.setLineWidth(300);
+            backButtonText.setTextAlign(TextAlign.CENTER | TextAlign.MIDDLE);
+            backButtonText.addConstraint(new VerticalCenterConstraint());
+            backButtonText.addConstraint(new HorizontalCenterConstraint());
+            createWorld.add(backButtonText);
+
+            createWorld.addUActionEvent(UIHoverEnterEvent.class, (UIHoverEnterEvent) vector2 -> createWorld.setColor(new RGBA(204, 202, 202, 0.5f)));
+            createWorld.addUActionEvent(UIHoverLeaveEvent.class, (UIHoverLeaveEvent) vector2 -> createWorld.setColor(new RGBA(255, 255, 255, 0.5f)));
+            createWorld.addUActionEvent(UIClickEvent.class, (UIClickEvent) (vector2, mouseClickType) -> goBack(createWorld));
+
+            componentCanvas.add(createWorld);
+        }
+        add(componentCanvas);
+
+    }
+
+    private void goBack(Rectangle backButton) {
+        try {
+            Scene scene = kakaraGame.firstScene(gameHandler);
+            gameHandler.getSceneManager().setScene(scene);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 

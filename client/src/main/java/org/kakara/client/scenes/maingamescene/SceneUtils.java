fd21619fb6cb3c4597cf2d5dfbe1ea63db1696d0
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-02-28 19:38:01 -0500

    Updated Engine

--- client/src/main/java/org/kakara/client/scenes/maingamescene/SceneUtils.java
+++ client/src/main/java/org/kakara/client/scenes/maingamescene/SceneUtils.java
@@ -1,11 +1,13 @@
 package org.kakara.client.scenes.maingamescene;
 
+import org.kakara.core.common.Kakara;
+import org.kakara.engine.components.MeshRenderer;
 import org.kakara.engine.gameitems.GameItem;
-import org.kakara.engine.gameitems.MeshGameItem;
 import org.kakara.engine.gameitems.mesh.Mesh;
 import org.kakara.engine.math.Vector3;
 import org.kakara.engine.models.StaticModelLoader;
 import org.kakara.engine.physics.collision.BoxCollider;
+import org.kakara.engine.physics.collision.PhysicsComponent;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -21,6 +23,7 @@ public class SceneUtils {
         return gameScene.getItemHandler().getItemWithId(uuid);
     }
 
+
     protected UUID createPlayerObject() {
         Mesh[] mainPlayer = null;
         try {
@@ -28,22 +31,26 @@ public class SceneUtils {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        MeshGameItem object = new MeshGameItem(mainPlayer);
-        object.setVisible(false);
-        object.setPosition((float) gameScene.getServer().getPlayerEntity().getLocation().getX(), (float) gameScene.getServer().getPlayerEntity().getLocation().getY(), (float) gameScene.getServer().getPlayerEntity().getLocation().getZ());
-        BoxCollider boxCollider = new BoxCollider(new Vector3(0, 0, 0), new Vector3(0.99f, 1.99f, 0.99f));
+        GameItem object = new GameItem(mainPlayer);
+        object.getComponent(MeshRenderer.class).setVisible(false);
+        object.transform.setPosition((float) gameScene.getServer().getPlayerEntity().getLocation().getX(), (float) gameScene.getServer().getPlayerEntity().getLocation().getY(), (float) gameScene.getServer().getPlayerEntity().getLocation().getZ());
+        PhysicsComponent physicsComponent = object.addComponent(PhysicsComponent.class);
+        BoxCollider boxCollider = object.addComponent(BoxCollider.class);
+        boxCollider.setPoint1(new Vector3(0, 0, 0));
+        boxCollider.setPoint2(new Vector3(0.99f, 1.99f, 0.99f));
         boxCollider.setPredicate(collidable -> {
-            if (collidable instanceof MeshGameItem) {
-                if (((MeshGameItem) collidable).getTag().equals("pickupable")) {
-                    return true;
-                }
-            }
-            //TODO check if bottom of player has touched the ground. Currently just reset on any touch.
             gameScene.movement.resetJump();
+
+            if (collidable.getGameItem() == null) return false; //How?
+            if (collidable.getGameItem().getTag() == null) return false;
+            if (collidable.getGameItem().getTag().equals("pickupable")) {
+                return true;
+            }
+
             return false;
         });
-        object.setCollider(boxCollider);
         gameScene.add(object);
+        Kakara.LOGGER.debug("Player created with the UUID of "+ object.getUUID().toString());
         return object.getUUID();
     }
 }
